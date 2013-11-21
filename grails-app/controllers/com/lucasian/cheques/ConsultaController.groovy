d60140027ce6a197e95f64e123288d882a0e835b
Angel pimentel <angelpmza@gmail.com>
2013-11-21 17:38:17 -0600

    permitir busqueda de cheque indivudual

--- grails-app/controllers/com/lucasian/cheques/ConsultaController.groovy
+++ grails-app/controllers/com/lucasian/cheques/ConsultaController.groovy
@@ -8,17 +8,27 @@ class ConsultaController {
        }
        def buscar(){ 
               params.serie = params.serie.trim()
-              def chequera = Chequera.findByNumero(params.serie)
-              
+              def partes = params.serie.split("-")
+              def isCompuesto =params.serie.contains("-")
+              println(partes)
+              def chequera = Chequera.findByNumero(partes[0])              
               if(chequera != null){
                      if(SecurityUtils.subject.hasRole("empleado")){
                             //results = Cheque.executeQuery("from Cheque c where c.serie.chequeras in (:cheq)", [cheq: chequeras])           
-                            def parametros = [claveTienda: SecurityUtils.subject.principal.tienda.clave, idSerie: chequera.serie.id]
                             def usados = ChequesUsados.executeQuery("from ChequesUsados c where c.chequera.id = :idChequera", [idChequera: chequera.id])                                                        
                             def usadosMap = usados.collectEntries { 
                                    [(it.cheque.id): it]
                             }
-                            def result = Cheque.executeQuery("from Cheque c where c.tienda.clave = :claveTienda and c.serie.id = :idSerie",parametros )                            
+                            def result
+                            def parametros
+                            if(isCompuesto){
+                                   parametros = [claveTienda: SecurityUtils.subject.principal.tienda.clave, idSerie: chequera.serie.id, claveCheque:partes[1]]
+                                   result = Cheque.executeQuery("from Cheque c where c.tienda.clave = :claveTienda and c.serie.id = :idSerie and c.clave=:claveCheque",parametros )                            
+                            }else{
+                                   parametros = [claveTienda: SecurityUtils.subject.principal.tienda.clave, idSerie: chequera.serie.id]
+                                   result = Cheque.executeQuery("from Cheque c where c.tienda.clave = :claveTienda and c.serie.id = :idSerie",parametros )                            
+
+                            }                            
                             def cheques = result.collect{
                                    if(usadosMap[it.id]){
                                           [cheque: it, usado: usadosMap[it.id]]
